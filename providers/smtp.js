7f7606f3b801e74b7dd1ce6bda48747383e43d13
Paul Aschmann <>
2022-07-15 10:24:23 -0400

    Add error handling to SMTP

--- providers/smtp.js
+++ providers/smtp.js
@@ -33,51 +33,54 @@ class SMTP extends provider {
     };
 
     async send(notification) {
-
-        const config = {
-            host: notification.required.smtpHost,
-            port: notification.required.smtpPort,
-            secure: notification.required.smtpSecure,
-            tls: {
-                rejectUnauthorized: notification.optional.smtpIgnoreTLSError || false,
+        try {
+            const config = {
+                host: notification.required.smtpHost,
+                port: notification.required.smtpPort,
+                secure: notification.required.smtpSecure,
+                tls: {
+                    rejectUnauthorized: notification.optional.smtpIgnoreTLSError || false,
+                }
+            };
+    
+            // Fix #1129
+            if (notification.optional.smtpDkimDomain) {
+                config.dkim = {
+                    domainName: notification.optional.smtpDkimDomain,
+                    keySelector: notification.optional.smtpDkimKeySelector,
+                    privateKey: notification.optional.smtpDkimPrivateKey,
+                    hashAlgo: notification.optional.smtpDkimHashAlgo,
+                    headerFieldNames: notification.optional.smtpDkimheaderFieldNames,
+                    skipFields: notification.optional.smtpDkimskipFields,
+                };
             }
-        };
-
-        // Fix #1129
-        if (notification.optional.smtpDkimDomain) {
-            config.dkim = {
-                domainName: notification.optional.smtpDkimDomain,
-                keySelector: notification.optional.smtpDkimKeySelector,
-                privateKey: notification.optional.smtpDkimPrivateKey,
-                hashAlgo: notification.optional.smtpDkimHashAlgo,
-                headerFieldNames: notification.optional.smtpDkimheaderFieldNames,
-                skipFields: notification.optional.smtpDkimskipFields,
-            };
+    
+            // Should fix the issue in https://github.com/louislam/uptime-kuma/issues/26#issuecomment-896373904
+            if (notification.optional.smtpUsername || notification.optional.smtpPassword) {
+                config.auth = {
+                    user: notification.optional.smtpUsername,
+                    pass: notification.optional.smtpPassword,
+                };
+            }
+    
+            let transporter = nodemailer.createTransport(config);
+    
+            // send mail with defined transport object
+            await transporter.sendMail({
+                from: notification.required.smtpFrom,
+                cc: notification.optional.smtpCC,
+                bcc: notification.optional.smtpBCC,
+                to: notification.required.smtpTo,
+                subject: notification.required.subject,
+                text: notification.required.text,
+                html: notification.optional.html,
+                attachments: notification.optional.attachments
+            });
+    
+            return "Message Sent";
+        } catch (err) {
+            return err;
         }
-
-        // Should fix the issue in https://github.com/louislam/uptime-kuma/issues/26#issuecomment-896373904
-        if (notification.optional.smtpUsername || notification.optional.smtpPassword) {
-            config.auth = {
-                user: notification.optional.smtpUsername,
-                pass: notification.optional.smtpPassword,
-            };
-        }
-
-        let transporter = nodemailer.createTransport(config);
-
-        // send mail with defined transport object
-        await transporter.sendMail({
-            from: notification.required.smtpFrom,
-            cc: notification.optional.smtpCC,
-            bcc: notification.optional.smtpBCC,
-            to: notification.required.smtpTo,
-            subject: notification.required.subject,
-            text: notification.required.text,
-            html: notification.optional.html,
-            attachments: notification.optional.attachments
-        });
-
-        return "Message Sent";
     }
 }
 
